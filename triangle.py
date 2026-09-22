bd22b14611efe9070145f618cc9b759e03108490
wopertac <wopertac@gmail.com>
2026-09-22 13:20:16 +0300

    Added rectangle.py triangle.py

---
+++ triangle.py
@@ -0,0 +1,30 @@
+def area(a : int|float, b : int|float, c : int|float) -> int|float:
+    '''
+    Вычисляет площадь треугольника со сторонами a, b, c
+
+    Параметры:
+        a (int|float) - сторона треугольника
+        b (int|float) - сторона треугольника
+        c (int|float) - сторона треугольника
+
+    Возвращает:
+        S (int|float) - площадь треугольника
+    '''
+
+    p = (a + b + c) / 2
+    return (p * (p - a) * (p - b) * (p - c)) ** .5
+
+def perimeter(a: int|float, b : int|float, c : int|float) -> int|float:
+    '''
+    Вычисляет периметр треугольника со сторонами a, b, c
+
+    Параметры:
+        a (int|float) - сторона треугольника
+        b (int|float) - сторона треугольника
+        c (int|float) - сторона треугольника
+
+    Возвращает:
+        P (int|float) - периметр треугольника
+    '''
+    
+    return a + b + c
